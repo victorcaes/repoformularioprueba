792864682a6c534f2c06f7f7e070b5f66724ac70
victor <eidos.caes@gmail.com>
2022-06-10 17:59:46 -0500

    2do formulario

---
+++ form2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>formulario 2</h2>
+    <p>formuaorio co subida de archivos</p>
+    
+    <div class="card">
+        <div class="card-header">
+            Header
+        </div>
+        <div class="card-body">
+            <form action="">
+                <input type="text" placeholder="nombre">
+                <input type="text" placeholder="apellido">
+                <input type="text" placeholder="numero de telefono">
+                <textarea name="comentarios" id="" cols="30" rows="10" placeholder="Escribe tu comentario"></textarea>
+                <input type="submit" value="Enviar">
+            </form>
+        </div>
+        <div class="card-footer text-muted">
+            
+        </div>
+    </div>
+    
+</body>
+</html>
