7e6661620635d29192725230b693e5dd47514270
victor <eidos.caes@gmail.com>
2022-06-10 18:12:53 -0500

    form2

---
+++ form2/proceso2.php
@@ -0,0 +1,23 @@
+<?php
+    $correo='eidos.caes@gmail.com';
+    $nombre=$_POST['nombre'];
+    $apellido=$_POST['apellido'];
+    $numero=$_POST['numero'];
+    $comentarios=$_POST['comentarios'];
+
+    $mensaje="nombre: ".$nombre."apellido: ".$apellido." numero: ".$numero."comentarios: ".$comentarios;
+
+    
+
+    
+    $email=mail($correo,'segundoform',$mensaje);
+
+    if($email){
+        echo "enviado correctamente"
+        
+    }else{
+        echo "no se ha enviado correctamente"
+    }
+
+
+?>
